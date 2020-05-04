85c8127afe3f8ef3451d50e2fcb4c7b9c40edfdd
skm <sckevmit@gmail.com>
2020-05-04 18:53:57 +0200

    Update config.py

--- src/cfnlint/config.py
+++ src/cfnlint/config.py
@@ -626,7 +626,7 @@ class ConfigMixIn(TemplateArgs, CliArgs, ConfigFileArgs, object):
     @property
     def output_file(self):
         """ output_file """
-        return self._get_argument_value('outputfile', False, True)
+        return self._get_argument_value('output_file', False, True)
 
     @property
     def update_specs(self):
